244a20ae6feabf7749f3a76974a48dc642f56083
Ksenia Stupina <k.stupina@okenit.ru>
2022-11-17 14:41:25 +0300

    add new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,15 +2,24 @@
 import UserPostsTypeScriptStart from "@/assets/user-posts-type-script-start.jpg";
 import MedicalNotesStart from "@/assets/medical-notes-start.jpg";
 import TestSiteStart from "@/assets/test-site-start.jpg";
+import ColorsStart from "@/assets/colors-start.jpg";
 import NotabStart from "@/assets/notab-start.jpg";
 import BeeStart from "@/assets/bee-start.jpg";
 import UserPostsTypeScriptEnd from "@/assets/user-posts-type-script-end.jpg";
 import MedicalNotesEnd from "@/assets/medical-notes-end.jpg";
 import TestSiteEnd from "@/assets/test-site-end.jpg";
+import ColorsEnd from "@/assets/colors-end.jpg";
 import NotabEnd  from "@/assets/notab-end.jpg";
 import BeeEnd  from "@/assets/bee-end.jpg";
 
 export const projectsCards = [
+    {
+        id: 6,
+        projectName: 'Colors',
+        imgStart: ColorsStart,
+        imgEnd: ColorsEnd,
+        url: 'https://stupinaki.github.io/colors/',
+    },
     {
         id: 3,
         projectName: 'Notab',
